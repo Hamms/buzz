ee840b4ad2643fe2b8161c417e6d05d5b0f0a072
Elijah Hamovitz <elijah@Elijahs-MacBook-Pro.local>
2012-05-29 13:59:10 -0700

    significantly cleaned up code, reformatted event handling, fixed a bug where false options would revert to defaults

--- AudioClip.js
+++ AudioClip.js
@@ -6,6 +6,70 @@ var AudioClip = function(options){
 
 	var clip = {};
 
+	// event handlers
+	clip.addListener = function(event, cb){
+		if (options.events[event] && options.events[event] instanceof Array) {
+			options.events[event].push(cb);
+		} else {
+			options.events[event] = [cb];
+		}
+		return clip;
+	}
+
+	clip.dispatchEvent = function(event, args){
+		var listeners = options.events[event];
+		if (listeners && listeners instanceof Array) {
+			for (var i in listeners) {
+				listeners[i].apply(this, args);
+			}
+		}
+		return clip;
+	}
+
+	// support detection
+	clip.isHtml5Supported = function() {
+		return !!clip.audio.canPlayType;
+	}
+
+	clip.isFormatSupported = function(format){
+		if (!clip.isHtml5Supported()){
+			return false;
+		} else if (format == 'ogg') {
+			return !!clip.audio.canPlayType('audio/ogg; codecs="vorbis"');
+		} else if (format == 'wav') {
+			return !!clip.audio.canPlayType('audio/wav; codecs="1"');
+		} else if (format == 'mp3') {
+			return !!clip.audio.canPlayType('audio/mpeg;');
+		} else if (format == 'aac') {
+			return !!(clip.audio.canPlayType('audio/x-m4a;') || clip.audio.canPlayType('audio/aac;'));
+		} else {
+			return false;
+		}
+	}
+
+	// API
+	clip.play = function(){
+		clip.audio.play();
+		return clip;
+	}
+	clip.pause = function(){
+		clip.audio.pause();
+		return clip;
+	}
+	clip.stop = function(clean){
+		clip.audio.pause();
+		if (clean) {
+			clip.unload();
+		} else {
+			clip.audio.currentTime = 0.0;
+		}
+		return clip;
+	}
+	clip.unload = function(){
+		throw new Error('unimplemented');
+		return clip;
+	}
+
 	//
 	// Init
 	//
@@ -23,84 +87,43 @@ var AudioClip = function(options){
 			// I'm not sure if I should do support checking, or if html
 			// wants to handle that itself.
 			for (var i in options.formats) {
-				var source = document.createElement('source');
-				source.src = options.src + '.' + options.formats[i];
-				clip.audio.appendChild(source);
+				if (clip.isFormatSupported(options.formats[i])) {
+					var source = document.createElement('source');
+					source.src = options.src + '.' + options.formats[i];
+					clip.audio.appendChild(source);
+				}
 			}
 		}
 	}
 	for (var i in defaults) {
-		if (defaults.hasOwnProperty(i)) {
-			options[i] = options[i] || defaults[i];
+		if (defaults.hasOwnProperty(i) && !options.hasOwnProperty(i)) {
+			options[i] = defaults[i];
 		}
 	}
 
-	if (options.autoplay) {
+	// option handling
+	if (options.autoPlay) {
 		clip.audio.autoplay = 'autoplay';
 	}
-	clip.audio.preload = 'auto';
+	if (options.autoLoad === true) {
+		clip.audio.preload = 'auto';
+	} else if (options.autoLoad === false) {
+		clip.audio.preload = 'none';
+	} else {
+		clip.audio.preload = options.preload;
+	}
 
 	// bind events
-	var audioEvents = {
-		'playing' : 'play',
-		'pause' : 'pause',
-		'ended' : 'complete',
-	};
-	for (var e in audioEvents) {
-		clip.audio.addEventListener(e, function(){
-			clip.dispatchEvent(audioEvents[e], arguments);
-		})
-	}
+	clip.audio.addEventListener('playing', function(){ clip.dispatchEvent('play', arguments); })
+	clip.audio.addEventListener('pause', function(){ clip.dispatchEvent('pause', arguments); })
+	clip.audio.addEventListener('ended', function(){ clip.dispatchEvent('complete', arguments); })
 
 	// set up clip event handlers
-	clip.events = {};
-
-	clip.addListener = function(event, cb){
-		if (clip.events[event] && clip.events[event].length) {
-			clip.events[event].push(cb);
-		} else {
-			clip.events[event] = [cb];
+	options.events = options.events || {};
+	for (var event in options.events) {
+		if (!(options.events[event] instanceof Array)) {
+			options.events[event] = [options.events[event]];
 		}
-	}
-
-	clip.dispatchEvent = function(event, args){
-		var listeners = clip.events[event];
-		if (listeners && listeners.length) {
-			for (var i in listeners) {
-				listeners[i].apply(this, args);
-			}
-		}
-	}
-
-	clip.play = function(){
-		clip.audio.play();
-	}
-	clip.pause = function(){
-		clip.audio.pause();
-	}
-	clip.stop = function(clean){}
-	clip.unload = function(){}
-
-	// various support functions ... I don't know how many of these are
-	// actually necessary
-	clip.isHtml5Supported = function() {
-		return !!clip.audio.canPlayType;
-	}
-
-	clip.isOGGSupported = function() {
-		return !!clip.audio.canPlayType && clip.audio.canPlayType( 'audio/ogg; codecs="vorbis"' );
-	}
-
-	clip.isWAVSupported = function() {
-		return !!clip.audio.canPlayType && clip.audio.canPlayType( 'audio/wav; codecs="1"' );
-	}
-
-	clip.isMP3Supported = function() {
-		return !!clip.audio.canPlayType && clip.audio.canPlayType( 'audio/mpeg;' );
-	}
-
-	clip.isAACSupported = function() {
-		return !!clip.audio.canPlayType && ( clip.audio.canPlayType( 'audio/x-m4a;' ) || clip.audio.canPlayType( 'audio/aac;' ) );
 	}
 
 	return clip;
